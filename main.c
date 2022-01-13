82277b37ab876bb4252b7b5cb6f43c88529605e0
bayamasa <mapi0213mgt@gmail.com>
2022-01-13 14:48:00 +0900

    put_pixel

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ typedef struct	s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
+	char	*path;
 	int		line_length;
 	int		endian;
 }				t_data;
@@ -11,14 +12,20 @@ typedef struct	s_data {
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 	void	*img;
-	char	*relative_path = "./chara.xpm";
 	int		img_width;
 	int		img_height;
+	t_data	chara;
+	t_data	wall;
+	t_data	floor;
 
-	img_height = 64;
-	img_width = 64;
 	mlx = mlx_init();
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	
+	mlx_win = mlx_new_window(mlx, 400, 400, "so_long");
+	chara.path = "./chara.xpm";
+	chara.img = mlx_xpm_file_to_image(mlx, chara.path, &chara.bits_per_pixel, &chara.line_length);
+	// wall.img = mlx_xpm_file_to_image(mlx, )
+	// chara.addr = 
+	mlx_put_image_to_window(mlx, mlx_win, chara.img, 0, 0);
+	mlx_loop(mlx);
 }
