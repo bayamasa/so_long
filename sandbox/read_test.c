95ef22dd077c14807d258aaf814452f3b00e7f40
bayamasa <mapi0213mgt@gmail.com>
2022-01-14 14:47:19 +0900

    add_method

--- sandbox/read_test.c
+++ sandbox/read_test.c
@@ -35,14 +35,67 @@ size_t	count_line_num(char *filepath)
 	return (i);
 }
 
+void	print_map(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		printf("one_line : %s\n", map[i]);
+		// while (map[i][j] != '\n' && map[i][j] != '\0')
+		// {
+		// 	printf("map[i][j] = %c\n", map[i][j]);
+		// 	j++;
+		// }
+		i++;
+	}
+}
+
+char	**store_all_line(char *filepath, char **map)
+{
+	int		fd;
+	char	*line;
+	int		status;
+	int		h;
+
+	h = 0;
+	status = true;
+	fd = open(filepath, O_RDONLY);
+	if (fd == -1)
+		abort_so_long(NULL, map);
+	while (true)
+	{
+		line = get_next_line(fd, &status);
+		if (status == false)
+			abort_so_long(line, map);
+		if (line == NULL)
+		{
+			// NULL止めしないと"a.out"の文字列が出てしまう。なぜ
+			map[h] = NULL;
+			break ;
+		}
+		printf("line : %s\n", line);
+		map[h] = line;
+		h++;
+	}
+	print_map(map);
+	return (map);
+}
+
+
 char	**store_map_from_file(char *filepath)
 {
 	int		fd;
 	char	*line;
 	char	**map;
+	size_t	line_num;
 
+	line_num = count_line_num(filepath);
+	map = (char **)malloc(sizeof(char *) * line_num);
 
-	
 }
 
 
@@ -51,6 +104,6 @@ int	main()
 	char **map;
 	char *filepath = "files/one_line";
 
-	// printf("count_line_num(filepath) = %zu\n", count_line_num(filepath));
+	store_all_line(filepath, map);
 	return (0);
 }
