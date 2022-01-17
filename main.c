2b531032e210ecf32ccf5d4c4a33e1b05124a907
bayamasa <mapi0213mgt@gmail.com>
2022-01-17 21:09:19 +0900

    done_move_player

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 15:02:44 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/01/17 20:19:25 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/01/17 21:04:03 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	get_player_grid(char **map, int *x, int *y)
 	while (map[h] != NULL)
 	{
 		w = 0;
-		while (map[h][w] != '\0' || map[h][w] != '\n' )
+		while (map[h][w] != '\0' && map[h][w] != '\n')
 		{
 			if (map[h][w] == 'P')
 			{
@@ -40,45 +40,64 @@ int	is_wall_in_the_direction(int code, t_data d)
 	int	x;
 	int	y;
 
-	print_map(d.map);
 	get_player_grid(d.map, &x, &y);
 	if (code == UP)
 		// x座標そのまま。y座標一つ上
-		if (d.map[x][y - 1] == '1')
+		if (d.map[y - 1][x] == '1')
 			return (true);
 	if (code == DOWN)
 		// x座標そのまま。y座標一つ上
-		if (d.map[x][y + 1] == '1')
+		if (d.map[y + 1][x] == '1')
 			return (true);
 	if (code == RIGHT)
-		if (d.map[x + 1][y] == '1')
+		if (d.map[y][x + 1] == '1')
 			return (true);
 	if (code == LEFT)
-		if (d.map[x - 1][y] == '1')
+		if (d.map[y][x - 1] == '1')
 			return (true);
 	return (false);
 }
 
-void	rerender_player(int code, t_data d)
+void	move_player_grid(int code, char **map, int x, int y)
+{
+	map[y][x] = '0';
+	if (code == UP)
+		map[y - 1][x] = 'P';
+	if (code == DOWN)
+		map[y + 1][x] = 'P';
+	if (code == RIGHT)
+	{
+		map[y][x + 1] = 'P';
+	}
+	if (code == LEFT)
+	{
+		map[y][x - 1] = 'P';
+	}
+}
+
+void	rerender_player(int code, t_data *d)
 {
 	int	x;
 	int	y;
 
-	get_player_grid(d.map, &x, &y);
-	mlx_put_image_to_window(d.mlx, \
-			d.mlx_win, d.obj.floor.img, x * 50, y * 50);
+	get_player_grid(d->map, &x, &y);
+	// printf("x = %d\n", x);
+	// printf("y = %d\n", y);
+	mlx_put_image_to_window(d->mlx, \
+			d->mlx_win, d->obj.floor.img, x * 50, y * 50);
 	if (code == UP)
-		mlx_put_image_to_window(d.mlx, \
-			d.mlx_win, d.obj.player.img, x * 50, (y - 1) * 50);
+		mlx_put_image_to_window(d->mlx, \
+			d->mlx_win, d->obj.player.img, x * 50, (y - 1) * 50);
 	if (code == DOWN)
-		mlx_put_image_to_window(d.mlx, \
-			d.mlx_win, d.obj.player.img, x * 50, (y + 1) * 50);	
+		mlx_put_image_to_window(d->mlx, \
+			d->mlx_win, d->obj.player.img, x * 50, (y + 1) * 50);
 	if (code == RIGHT)
-		mlx_put_image_to_window(d.mlx, \
-			d.mlx_win, d.obj.player.img, (x + 1) * 50, y * 50);
+		mlx_put_image_to_window(d->mlx, \
+			d->mlx_win, d->obj.player.img, (x + 1) * 50, y * 50);
 	if (code == LEFT)
-		mlx_put_image_to_window(d.mlx, \
-			d.mlx_win, d.obj.player.img, (x - 1) * 50, y * 50);
+		mlx_put_image_to_window(d->mlx, \
+			d->mlx_win, d->obj.player.img, (x - 1) * 50, y * 50);
+	move_player_grid(code, d->map, x, y);
 }
 
 int	move_player_hook(int keycode, void *data)
@@ -89,17 +108,39 @@ int	move_player_hook(int keycode, void *data)
 	if (keycode == KEY_ESC)
 		exit(0);
 	if (keycode == KEY_W)
+	{
 		if (!is_wall_in_the_direction(UP, *d))
-			rerender_player(UP, *d);
+		{
+			printf("player UP\n");
+			rerender_player(UP, d);
+		}
+	}
 	if (keycode == KEY_S)
+	{
 		if (!is_wall_in_the_direction(DOWN, *d))
-			rerender_player(UP, *d);
+		{
+			printf("player DOWN\n");
+			rerender_player(DOWN, d);
+		}
+	}
 	if (keycode == KEY_A)
+	{
 		if (!is_wall_in_the_direction(LEFT, *d))
-			rerender_player(LEFT, *d);
+		{
+			printf("player LEFT\n");
+			rerender_player(LEFT, d);
+		}
+	}
 	if (keycode == KEY_D)
+	{
 		if (!is_wall_in_the_direction(RIGHT, *d))
-			rerender_player(RIGHT, *d);
+		{
+			printf("player RIGHT\n");
+			rerender_player(RIGHT, d);
+		}
+	}
+	printf("hook_end\n");
+	print_map(d->map);
 	return (0);
 }
 
@@ -117,7 +158,6 @@ int	main(void)
 	data.mlx = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx, 400, 400, "so_long");
 	data = put_pixel_by(data.map, data);
-	print_map(data.map);
 	mlx_hook(data.mlx_win, X_EVENT_KEY_PRESS, 0, move_player_hook, &data);
 	mlx_loop(data.mlx);
 	// free_all(NULL, map);
